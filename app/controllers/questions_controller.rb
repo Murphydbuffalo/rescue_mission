95a3c87d431f0f7afe940eac955ffa7dbfa14df7
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-16 23:03:45 -0400

    Set up application.html.erb template with a header, nav bar, and div for flash notices.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,27 +14,33 @@ class QuestionsController < ApplicationController
 
 	def create
 	  @question = Question.create(question_params)
+	  if @question.save
+	  	redirect_to '/'
+	  else
+	  	flash[:notice] = "We'll eventually put specific error messages here!"
+	    render :new
+	  end
+	end
+
+	def edit
+	  @question = Question.new
+	end
+
+	def update
+	  @question = Question.find(params[:id]).update(question_params)
+	  redirect_to "/questions/#{params[:id]}"
+	end
+
+	def destroy
+	  Question.find(params[:id]).destroy
 	  redirect_to '/'
 	end
 
-end
-def edit
-  @question = Question.new
-end
 
-def update
-  @question = Question.find(params[:id]).update(question_params)
-  redirect_to "/questions/#{params[:id]}"
-end
+	private
 
-def destroy
-  Question.find(params[:id]).destroy
-  redirect_to '/'
-end
+	def question_params
+	  params.require(:question).permit(:title, :body, :user_id)
+	end
 
-
-private
-
-def question_params
-  params.require(:question).permit(:title, :body, :user_id)
 end
