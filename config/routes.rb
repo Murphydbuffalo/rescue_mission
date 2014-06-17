af31921a2f1340fb7027ccad9f708cb9baea0827
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-16 22:35:43 -0400

    Called resources, instead of resource, to create necessary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'questions#index'
 
-  resource :questions do
-    resource :answers
+  resources :questions do
+    resources :answers
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
